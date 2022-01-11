645bcb74a0c6fd26556c33729bb71299f280efff
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-01-11 19:48:10 +0100

    update

--- api/app.py
+++ api/app.py
@@ -13,5 +13,13 @@ def select():
         "data": dump
     }
 
+@app.route('/temp/add/<value>')
+def add(value):
+    conn = con()
+    create_(conn, "insert into sensor.temperature (value) values ({});".format(value))
+    return {
+        "data": "ok"
+    }
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=4000)
--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+import requests
 
+class Temperature:
+    def __init__(self, url="http://54.235.226.52:4000/temp/"):
+        self.infos = "Class for connection to the MariaDB"
+        self.url = url
+
+    def select(self):
+        response = requests.get(self.url+"select")
+        return response.json()
+    def insert(self, data):
+        response = requests.get(self.url+"select")
+        return response.json()
 
 if __name__ == '__main__':
-    pass
+    temp = Temperature()
+    print(temp.select())
