d9635362f72feaa0cf03a0c4c6f6f094ffb279ff
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-01-11 18:53:39 +0100

    update

---
+++ api/operation.py
@@ -0,0 +1,52 @@
+# Module Imports
+import mariadb
+import sys
+
+def execute(conn, sql):
+   cursor= conn.cursor()
+   cursor.execute(sql)
+   row_headers = [value[1] for value in cursor]
+
+   return row_headers
+
+def create_(conn, sql):
+   cursor= conn.cursor()
+   cursor.execute(sql)
+   conn.commit()
+   cursor.close()
+
+def update_(conn, sql):
+   create_(conn, sql)
+
+
+
+try:
+   conn = conn = mariadb.connect(
+      user="root",
+      password="password",
+      host="localhost",
+      ssl_ca="skysql_chain.pem",
+      port=5009)
+   # create database
+   #create_(conn, "CREATE DATABASE sensor")
+
+   print("inserting value")
+   for i in range(30,44):
+      #create_(conn, "insert into sensor.temperature (value) values ({});".format(i))
+      pass
+
+   print("select operations")
+   temperatures = execute(conn, "SELECT * FROM sensor.temperature;")
+   print([int(t) for t in temperatures])
+
+   print("update operations")
+   update_(conn, "UPDATE sensor.temperature SET value={} where id={};".format(75, 5))
+
+
+   print("select operations")
+   temperatures = execute(conn, "SELECT * FROM sensor.temperature;")
+   print([int(t) for t in temperatures])
+
+except mariadb.Error as e:
+   print(f"Error connecting to MariaDB Enterprise: {e}")
+   sys.exit(1)
