ddbedbaa0e0bfdd7e8af8dfcab718e1e240d54c0
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-01-11 19:13:54 +0100

    Added operation function for the API

--- api/app.py
+++ api/app.py
@@ -1,13 +1,17 @@
 # Add this to app.py
 from flask import Flask
+from operation import *
 
 app = Flask(__name__)
 
-@app.route('/temperature/select')
+@app.route('/temp/select')
 def select():
-        return {
-                "data": []
-        }
+    conn = con()
+    temperatures = execute(conn, "SELECT * FROM sensor.temperature;")
+    dump = [int(t) for t in temperatures]
+    return {
+        "data": dump
+    }
 
 if __name__ == "__main__":
-        app.run(host="0.0.0.0",port=4000)
+    app.run(host="0.0.0.0",port=4000)
--- api/operation.py
+++ api/operation.py
@@ -18,35 +18,37 @@ def create_(conn, sql):
 def update_(conn, sql):
    create_(conn, sql)
 
-
-
-try:
-   conn = conn = mariadb.connect(
+def con():
+    return mariadb.connect(
       user="root",
       password="password",
       host="localhost",
       ssl_ca="skysql_chain.pem",
       port=5009)
-   # create database
-   #create_(conn, "CREATE DATABASE sensor")
 
-   print("inserting value")
-   for i in range(30,44):
-      #create_(conn, "insert into sensor.temperature (value) values ({});".format(i))
-      pass
+if __name__ == "__main__":
+    try:
+       conn = con()
+       # create database
+       #create_(conn, "CREATE DATABASE sensor")
 
-   print("select operations")
-   temperatures = execute(conn, "SELECT * FROM sensor.temperature;")
-   print([int(t) for t in temperatures])
+       print("inserting value")
+       for i in range(30,44):
+          #create_(conn, "insert into sensor.temperature (value) values ({});".format(i))
+          pass
 
-   print("update operations")
-   update_(conn, "UPDATE sensor.temperature SET value={} where id={};".format(75, 5))
+       print("select operations")
+       temperatures = execute(conn, "SELECT * FROM sensor.temperature;")
+       print([int(t) for t in temperatures])
+
+       print("update operations")
+       update_(conn, "UPDATE sensor.temperature SET value={} where id={};".format(75, 5))
 
 
-   print("select operations")
-   temperatures = execute(conn, "SELECT * FROM sensor.temperature;")
-   print([int(t) for t in temperatures])
+       print("select operations")
+       temperatures = execute(conn, "SELECT * FROM sensor.temperature;")
+       print([int(t) for t in temperatures])
 
-except mariadb.Error as e:
-   print(f"Error connecting to MariaDB Enterprise: {e}")
-   sys.exit(1)
+    except mariadb.Error as e:
+       print(f"Error connecting to MariaDB Enterprise: {e}")
+       sys.exit(1)
